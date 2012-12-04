8d4af4e424daf2a1754144cb244b643b1ebefb1e
Jonathan Ku <jonathanku88@gmail.com>
2012-12-03 19:42:42 -0500

    jon

--- js/flow.js
+++ js/flow.js
@@ -1,7 +1,7 @@
 $(function() {
         $("#controller").jFlow({
             slides: "#slides",
-            width: "900px",
+            width: "960px",
             height: "300px",
             
             
