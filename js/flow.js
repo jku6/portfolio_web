a00939c762b519546df16d96bd399b3ad8f6424d
Jonathan Ku <jonathanku88@gmail.com>
2012-12-03 19:13:50 -0500

    major changes to style

---
+++ js/flow.js
@@ -0,0 +1,10 @@
+$(function() {
+        $("#controller").jFlow({
+            slides: "#slides",
+            width: "900px",
+            height: "300px",
+            
+            
+
+        });
+    });
